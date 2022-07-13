ed0fb42c9daf5e34d320b6d8ccfab2e60f62b817
Anton Antonov <tonik32@gmail.com>
2022-07-13 21:22:38 +0300

    Добавляет переносы

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from http import HTTPStatus
 
 
-
 def index(request):
     return HttpResponse(
         'У меня получилось!',
         status=HTTPStatus.OK
     )
+
 
 def second_page(request):
     return HttpResponse(
