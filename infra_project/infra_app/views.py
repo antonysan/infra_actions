1149ac08d902adf5145b9789bf1824d91e83037e
Anton Antonov <tonik32@gmail.com>
2022-07-13 21:19:15 +0300

    Исправляет форматирование

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -4,8 +4,14 @@ from http import HTTPStatus
 
 
 def index(request):
-    return HttpResponse('У меня получилось!', status=HTTPStatus.OK)
+    return HttpResponse(
+        'У меня получилось!',
+        status=HTTPStatus.OK
+    )
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница', status=HTTPStatus.OK)
+    return HttpResponse(
+        'А это вторая страница',
+        status=HTTPStatus.OK
+    )
